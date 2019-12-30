bb841f8e2dbe3f0fe0f36c223fe8e9a9deb1498e
SirFagalot <candyfluzz13@gmail.com>
2019-12-30 09:18:07 +0100

    Test

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.login("NjU1MjE1ODAxNzA3NjU5MzIx.XgmiLA.e9nSLmBvQiNHnx10_EOc6kHWyO4");
+client.login(process.env.token);
 
 client.on('ready', () => {
     console.log("Booted up!");
@@ -10,7 +10,7 @@ client.on('ready', () => {
 client.on('message', message => {
   if(message.author.bot)
         return;
-    
+
     if(message.content.toLowerCase() === 'hello')
         message.channel.send("Hey!");
 });
