403ddc65b7651d984dcf7e00e14b0b8722d1c0d3
SirFagalot <candyfluzz13@gmail.com>
2019-12-30 08:21:19 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-client.on('ready', function() {
+client.login("NjU1MjE1ODAxNzA3NjU5MzIx.XgmiLA.e9nSLmBvQiNHnx10_EOc6kHWyO4")
+
+client.on('ready', () {
     console.log('Booted up!');
 });
 
-client.on('message', (message) -> {
-  if(message.content == 'ping'){
-        message.reply('PONG!');
+client.on('message', message -> {
+  if(message.author.bot)
+        return;
+    
+    if(message.content.toLowerCase() === 'hello')
+        message.channel.send("Hey!");
 });
-
-client.login(process.env.BOT_TOKEN);
