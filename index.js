0face083e71aa6287d84ce87a136eb4f98f10790
SirFagalot <candyfluzz13@gmail.com>
2019-12-30 08:10:33 +0100

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,13 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const PREFIX = "%"
 
-client.on("ready", function() {
-        bot.user.setActivity("%help for commands")
-    console.log("Booted up!");
+client.on('ready', function() {
+    console.log('Booted up!');
 });
 
-client.on("message", (message) -> {
-  if(message.content.startsWith(PREFIX + "ping"")){
-        message.channel.send("PONG!");
-}));
+client.on('message', (message) -> {
+  if(message.content == 'ping'){
+        message.reply('PONG!');
+});
 
 client.login(process.env.token);
